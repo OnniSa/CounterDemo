d1011ba0a934b9a76a7e8990a7ac94d32082850d
Salonen Onni <s1900566@edu.bc.fi>
2019-10-11 10:49:07 +0300

    final

---
+++ src/components/CounterHook.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import './CounterHook.css';
+
+const CounterHook = () => {
+  const [count, setCount] = useState(0);
+
+  const addHandler = () => {
+    setCount(count + 1);
+  }
+
+  const removeHandler = () => {
+    count > 0 ? setCount(count - 1) : setCount(0);
+  }
+
+  const resetHandler = () => {
+    setCount(0);
+  }
+
+  return (
+    <>
+      <div className={count === 0 ? 'counter zero' :
+        count % 2 === 0 ? 'counter even' : 'counter odd'
+      }>
+        <p className="number">{count}</p>
+      </div>
+
+      <div className="buttons">
+        <button className="add button" id="add-button" onClick={addHandler}>Add</button>
+        <button className="remove button" id="remove-button" onClick={removeHandler}>Remove</button>
+        <button className="reset button" id="reset-button" onClick={resetHandler}>Reset</button>
+      </div>
+    </>
+  )
+
+}
+
+export default CounterHook;
